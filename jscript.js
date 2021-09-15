0f362988ce2bca506d9fa9b1e34da65479541878
standon1006 <89518445+standon1006@users.noreply.github.com>
2021-09-15 18:38:23 +0530

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,3 +1,10 @@
 // javascript
-d3.select();
-d3.selectAll();
+var dataset = [1, 2, 3, 4, 5];
+
+d3.select('body')
+    .selectAll('p')
+    .data(dataset)
+    .enter()
+    .append('p') // appends paragraph for each data element
+    //.text('D3 is awesome!!');
+    .text(function(d) { return d; });
